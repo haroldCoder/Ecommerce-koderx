4dc24c4f4a286077cd3adb28f3069896b7129ad6
haroldCoder <haroldc2005@gmail.com>
2023-08-19 16:57:55 -0500

    config server

---
+++ backend/src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json())
+
+app.listen(4500, ()=>{
+    console.log(`Server on port 4500`);
+})
