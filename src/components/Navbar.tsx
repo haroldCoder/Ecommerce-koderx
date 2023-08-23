4498c57ce5be18c73af640a2f6d89b9701b70937
haroldCoder <haroldc2005@gmail.com>
2023-08-23 13:23:43 -0500

    add clerk and panel login

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,6 +9,7 @@ import AccountCircle from '@mui/icons-material/AccountCircle';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
 import { Button} from '@mui/material';
+import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/clerk-react';
 
 export default function Navbar() {
     const [auth] = React.useState(true);
@@ -78,38 +79,12 @@ export default function Navbar() {
                         </div>
                     </div>
                     
-                    {auth && (
-                        <div>
-                            <IconButton
-                                size="large"
-                                aria-label="account of current user"
-                                aria-controls="menu-appbar"
-                                aria-haspopup="true"
-                                onClick={handleMenu}
-                                color="inherit"
-                            >
-                                <AccountCircle />
-                            </IconButton>
-                            <Menu
-                                id="menu-appbar"
-                                anchorEl={anchorEl}
-                                anchorOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'right',
-                                }}
-                                keepMounted
-                                transformOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'right',
-                                }}
-                                open={Boolean(anchorEl)}
-                                onClose={handleClose}
-                            >
-                                <MenuItem onClick={handleClose}>Profile</MenuItem>
-                                <MenuItem onClick={handleClose}>My account</MenuItem>
-                            </Menu>
-                        </div>
-                    )}
+                    <SignedIn>
+                        <UserButton afterSignOutUrl={window.location.href} />
+                    </SignedIn>
+                    <SignedOut>
+                        <SignInButton mode='modal' />
+                    </SignedOut>
                 </Toolbar>
             </AppBar>
         </Box>
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './index.css'
 import { ClerkProvider } from '@clerk/clerk-react'
 
 const publicKey = import.meta.env.VITE_CLERK_KEY
+console.log(publicKey);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
