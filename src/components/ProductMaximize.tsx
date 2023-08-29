39a9305f21c7d6e3f6284f84e9ad63dbdb6e402c
haroldCoder <haroldc2005@gmail.com>
2023-08-29 12:27:30 -0500

    reduce code

--- src/components/ProductMaximize.tsx
+++ src/components/ProductMaximize.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react"
+import { useMemo, useState } from "react"
 import { Products } from "../types"
 import axios from "axios";
 import {useParams} from 'react-router-dom'
