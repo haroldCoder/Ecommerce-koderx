bead5f345fdccec6531287a00d466be278d74c85
haroldCoder <haroldc2005@gmail.com>
2023-08-29 11:10:34 -0500

    animation in home

--- src/components/CardProducts.tsx
+++ src/components/CardProducts.tsx
@@ -8,14 +8,14 @@ interface propsCard{
 
 export default function CardProducts ({id, name, description, imgURI, price} : propsCard) {
   return (
-    <div onClick={()=>location.href = `/cards/${id}`} className='bg-dark rounded-md w-[23%] mb-5 cursor-pointer mx-3 bg-[#c5c5c559] backdrop-blur-lg'>
+    <div onClick={()=>location.href = `/cards/${id}`} className='bg-dark rounded-md w-[23%]  mb-5 cursor-pointer mx-3 bg-[#c5c5c559] backdrop-blur-lg'>
         <section className='title flex justify-between px-4 py-2 bg-gray-500 rounded-t-md'>
             <h1 className='text-white text-base'>{name}</h1>
             <h2 className='text-gray-700'>{price}$</h2> 
         </section>
         <div className='hover:bg-gradient-to-tr from-black via-black to-blue-800 rounded-b-md h-[95%]'>
             <section className='px-4 pt-8 pb-4'>
-                <img src={imgURI} className='w-[auto] h-[30vh] hover:scale-105 rounded-md mx-auto' alt='name' />
+                <img src={imgURI} className='w-[auto] h-[30vh] hover:scale-105 duration-150 rounded-md mx-auto' alt='name' />
                 <p className='text-gray-400 mt-5'>{description}</p>
             </section>
         </div>
