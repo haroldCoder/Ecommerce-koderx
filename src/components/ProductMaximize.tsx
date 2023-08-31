80f459d75947a3ddd83c8f01ad13898cc6a2c22e
haroldCoder <haroldc2005@gmail.com>
2023-08-31 14:08:28 -0500

    is add product in carshop in product maximize

--- src/components/ProductMaximize.tsx
+++ src/components/ProductMaximize.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from "react"
+import { useEffect, useMemo, useRef, useState } from "react"
 import { Products } from "../types"
 import axios from "axios";
 import {useParams} from 'react-router-dom'
@@ -8,15 +8,33 @@ export default function ProductMaximize() {
     const [data, setData] = useState<Products>();
     const {id} = useParams<{id: string}>()
     const {user} = useUser();
+    const [isaddcar, setIsaddcar] = useState<boolean>(false)
     
     useMemo(()=>{
         const getData = async() =>{
             const res = (await axios.get(`${import.meta.env.VITE_API_URL}products/${id}?username=${import.meta.env.VITE_ACCESS}`)).data
-
             setData(res)
         }
+        
         getData();
     },[])
+
+    useEffect(()=>{
+        const isAddcar = async() =>{
+            console.log(user?.firstName);
+            
+            await axios.get(`${import.meta.env.VITE_API_URL}carshop/${id}/${user?.fullName}?username=${import.meta.env.VITE_ACCESS}`)
+            .then((res)=>{
+                if(res.status == 200){
+                    setIsaddcar(true)
+                }
+            })
+        }
+        if(user){
+          isAddcar();  
+        }
+        
+    },[user])
 
     const payProduct = async() =>{
         await axios.post(`https://stripe-node-microservice.vercel.app/api/stripe`,{
@@ -55,7 +73,12 @@ export default function ProductMaximize() {
                 <p className="text-green-300">by <span className="text-gray-300">{data?.author}</span></p>
             </div>
             <div className="flex mt-10 justify-between">
-                <button onClick={()=>reserveProduct(data?._id)} className="bg-blue-600 rounded-md hover:scale-105 text-gray-400 px-4 py-2 w-[45%]">add car</button>
+                
+                {
+                    !isaddcar ? 
+                        <button onClick={()=>reserveProduct(data?._id)} className="bg-blue-600 rounded-md hover:scale-105 text-gray-400 px-4 py-2 w-[45%]">add car</button>
+                        : <button onClick={payProduct} className="rounded-md text-white bg-green-600 cursor-default px-4 py-2 w-[45%]">This product ready add car</button>
+                }
                 <button onClick={payProduct} className="rounded-md text-white hover:bg-white hover:text-black px-4 py-2 w-[45%]">pay now</button> 
             </div>
             
